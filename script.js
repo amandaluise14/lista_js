e18fd51b2d485a42cfddc568af886d9421b531fa
amandaluise14 <amanda_l_maia@estudante.sesisenai.org.br>
2026-03-13 09:36:29 -0300

    ADD - finalizei o segundo exercício

--- script.js
+++ script.js
@@ -44,3 +44,4 @@ function somarNumeros(){
 }
 
 somarNumeros();
+
---
+++ script1.js
@@ -0,0 +1,7 @@
+console.log("Contagem regressiva para lançamento!");
+for(let i = 10; i >= 0; i--) {
+    console.log(i);
+}
+{
+    console.log("Lançamento realizado!");
+}
