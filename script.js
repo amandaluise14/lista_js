31b1b93feb580f6e69012b3c23d14b2a5844533c
amandaluise14 <amanda_l_maia@estudante.sesisenai.org.br>
2026-03-14 13:21:48 -0300

    ADD - realizei os exercicios 3 ate o 7.

--- script.js
+++ script.js
@@ -57,5 +57,99 @@ function exercicio2() {
 // exercicio2();
 
 function exercicio3(){
+   let ganhoAnual = 0;
+   let gastoAnual = 0;
+   
+   for(let i = 1; i <= 5; i++){
+    let ganhoMes = parseFloat(prompt("Digite o ganho bruto do mês " + i));
+    let gastoMes = parseFloat(prompt("Digite o gasto do mês " + i));
+
+    ganhoAnual += ganhoMes;
+    gastoAnual += gastoMes;
+   }
+
+   let saldoAnual = ganhoAnual - gastoAnual;
+
+    console.log("Ganho bruto Anual: " + ganhoAnual);
+    console.log("Gasto Anual: " + gastoAnual);
+    console.log("Saldo financeiro Anual: " + saldoAnual); 
+    
+    if(gastoAnual > ganhoAnual){
+        console.log("A empresa teve prejuízo");
+    }else if(gastoAnual < ganhoAnual){
+        console.log("A empresa teve lucro");
+    }else{
+        console.log("A empresa não teve lucro e nem prejuízo");
+    }
+}
+// exercicio3();
+
+function exercicio4(){
+
+    let n1 = parseInt(prompt("Digite um número inteiro"));
+    let n2 = parseInt(prompt("Digite um número inteiro"));
+    let n3 = parseInt(prompt("Digite um número inteiro"));
+    let n4 = parseInt(prompt("Digite um número inteiro"));
+
+    let numeros = [n1, n2 , n3, n4];
+
+    numeros.sort((a,b) => b - a);
+
+    console.log("Ordem decrescente:" + numeros);
     
 }
+// exercicio4();
+
+function exercicio5(){
+
+    let numero = parseInt(prompt("Digite um número inteiro"));
+
+    if(numero % 2 == 0){
+        numero += 1;
+    }else{
+        numero -= 1;
+    }
+
+    console.log("Valor final:" + numero);
+}
+// exercicio5();
+
+function exercicio6(){
+
+     let letra = prompt("Digite uma letra do alfabeto");
+
+     if(letra == "a" || letra == "e" || letra == "i" || letra == "o" || letra == "u"){
+        console.log("A sua letra escolhida é uma vogal.");
+     }else{
+        console.log("A sua letra escolhida é uma consoante.");
+     }
+}
+// exercicio6();
+
+function exercicio7(){
+
+    let sabor = prompt("a - Chocolate \nb - Morango \nc - Creme \nd - Manga \ne - Melancia \nf - Vanilla Ice \ng - Céu Azul \nh - Brownie \ni - Hawaiano \nEscolha um sabor e digite seu código:");
+
+    if(sabor == "a"){
+        console.log("Chocolate R$ 1,50");
+    }else if(sabor == "b"){
+        console.log("Morango R$ 2,50");
+    }else if(sabor == "c"){
+        console.log("Creme R$ 2,50");
+    }else if(sabor == "d"){
+        console.log("Manga R$ 3,20");
+    }else if(sabor == "e"){
+        console.log("Melancia R$ 3,40");
+    }else if(sabor == "f"){
+        console.log("Vanilla Ice R$ 3,00");
+    }else if(sabor == "g"){
+        console.log("Céu Azul R$ 3,60");
+    }else if(sabor == "h"){
+        console.log("Brownie R$ 4,00");
+    }else if(sabor == "i"){
+        console.log("Hawaiano R$ 5,00");
+    }else{
+        console.log("Código inválido");
+    }
+}
+// exercicio7();
